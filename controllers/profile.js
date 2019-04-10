9135d4016540dda2e2b078158ebed8fbfa0b5f76
Jonathan Ray <jonathanray17@gmail.com>
2019-04-09 23:38:29 -0400

    planning, plotting more of the site

--- controllers/profile.js
+++ controllers/profile.js
@@ -18,6 +18,13 @@ module.exports = {
     getProfile
 };
 
+// // Notes on Axios:
+// const axios = require('axios');
+// axios.{{METHOD}}('{{URL}}').then((response)=>{console.log(response)});
+// // example of GET and POST with options:
+// axios.get('/user', {params: {ID: 12345}}).then(function (response) {console.log(response);}).catch(function (error) {console.log(error);});
+// axios.post('/user', {firstName: 'Fred',lastName: 'Flintstone'}).then(function (response) {console.log(response);}).catch(function (error) {console.log(error);});
+// // there's even support for Promise.All: https://www.npmjs.com/package/axios#user-content-example
 
 // // API FRAMEWORK // //
 
