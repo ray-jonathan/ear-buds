2fd2ed9db542c5d0ca0e781b72181c7840a4f894
Jonathan Ray <jonathanray17@gmail.com>
2019-04-11 14:32:32 -0400

    maps user's matches to the screen, does some dynamic things

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,12 +1,13 @@
 // functions for res.render-ing user info from routes
-
 const  Message = require('../models/messages');
+const Profile = require('../models/profile');
+const Match = require('../models/match');
 
 async function getMessages(req, res){
     // const matches = await Message.getMatches(req.session.passport.user.id)
     // console.log(matches)
     // console.log(req.session.user)
-    const matchIdsForCurrentUser = await Message.getMatchId(2);
+    const matchIdsForCurrentUser = await Message.getMatchId(req.session.userid);
     // console.log("matchIdsForCurrentUser: ", matchIdsForCurrentUser);
     let allConversationsWithUser = [];
     for(let i = 0; i < matchIdsForCurrentUser.length; i++) { // forEach and map were giving us headache, back to basics
@@ -19,11 +20,35 @@ async function getMessages(req, res){
     const mostRecent = await Message.getMostRecentMessage(matchIdsForCurrentUser[1].id);
     // console.log("Most recent message: ",mostRecent);
     ////////////////////////////////////////////////////////////////////////////////////////////////////
+    const theUser = await Profile.getBySpotifyId(req.session.passport.user.id);
+    console.log(theUser);
+
+    let arrayOfOtherPeopleIds = [];
+    // need to pass in iterated profile info for other users as well
+    for(let i = 0; i < matchIdsForCurrentUser.length; i++) { // forEach and map were giving us headache, back to basics
+        let aMatch = await Match.getMatchById(matchIdsForCurrentUser[i].id);
+        if(!(theUser.id === aMatch.current_user_id)){
+            arrayOfOtherPeopleIds.push(aMatch.current_user_id);
+        }
+        else{
+            arrayOfOtherPeopleIds.push(aMatch.viewed_user_id);
+        }
+        // we want to push the other user's id # into the array
+    }
+    let arrayOfOtherPeople = [];
+    for(let i = 0; i < arrayOfOtherPeopleIds.length; i++) { // forEach and map were giving us headache, back to basics
+        const aUser = await Profile.getUserById(arrayOfOtherPeopleIds[i]);
+        arrayOfOtherPeople.push(aUser);
+    }
     res.render('./messages.html', {
         locals: { 
             // user: req.session.passport.user
+            // need to send the selecter conversation's compete info
+            // if possible, add ternary for adding "active" class of that conversation (maybe based on the URL path from before)
             recent: mostRecent,
             allMessages: allConversationsWithUser,
+            user: theUser,
+            otherUsers: arrayOfOtherPeople
             
 
         },
