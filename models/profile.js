259c5abc7c71084a1aebdb6864e451799daf5f2f
Jonathan Ray <jonathanray17@gmail.com>
2019-04-14 16:48:46 -0400

    fixes schema too

--- models/profile.js
+++ models/profile.js
@@ -1,20 +1,22 @@
 const db = require('./conn');
+const moment = require('moment');
 
 class Profile {
-    constructor(id, spotify_id, name, picture='http://beverlycove.org/wp-content/uploads/2016/07/no-profile-male.jpg'){
-        this.id = id;
+    constructor(id, spotify_id, name, picture='http://beverlycove.org/wp-content/uploads/2016/07/no-profile-male.jpg', last_vist){        this.id = id;
         this.spotifyId = spotify_id;
         this.name = name;
         this.picture = picture;
+        this.lastVist = last_vist;
     }
 
     static add(user){
         const photo = user.photos[0]? user.photos[0] : 'http://beverlycove.org/wp-content/uploads/2016/07/no-profile-male.jpg';
+        const now = moment().format();
         return db.one(`insert into users 
-        (spotify_id, name, picture)
+        (spotify_id, name, picture, last_vist)
         values
-        ($1,$2,$3)
-        returning true`,[user.id, user.displayName, photo]);
+        ($1,$2,$3,$4)
+        returning true`,[user.id, user.displayName, photo, now]);
     }
 
     static checkSpotifyID(spotifyId){
@@ -40,6 +42,11 @@ class Profile {
     // })
     static getUserById(id){
         return db.one(`select * from users where id=$1`, [id]);
+    }
+
+    static lastVist(id){
+        const now = moment().format();
+        return db.one(`update only users set last_vist = $1 where id=$2 returning true;`, [now, id]);
     }
 
 }
